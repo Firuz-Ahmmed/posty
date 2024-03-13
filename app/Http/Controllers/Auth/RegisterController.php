166b6d0961001e87f5bbf6013f58cc6cf2642205
Firuz Ahmmed <iamfiruz70@gmail.com>
2024-03-13 22:17:34 +0600

    Initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __construct(){
+        $this->middleware(['guest']);
+    }
+    public function index(){
+        return view("auth.register");
+    }
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+           
+        ]);
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->username = $request->input('username');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        auth()->attempt($request->only('email','password'));
+        return redirect()->route('dashboard');
+        
+    }
+}
